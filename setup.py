971a9c112c067d6f4f7065818d5970dced54ae16
Erik Telepovský <erik.telepovsky@gmail.com>
2020-07-18 00:09:16 +0200

    v1.2.0

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='mantis-cli',
-    version='1.1.0',
+    version='1.2.0',
     description='Management command to build and deploy webapps, especially based on Django',
     long_description=open('README.md').read(),
     author='Erik Telepovský',
