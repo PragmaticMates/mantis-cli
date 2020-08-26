4a90d13d6d9df0eddd3490d58d7495edf5c1c372
Erik Telepovský <erik.telepovsky@gmail.com>
2020-08-26 23:26:18 +0200

    waiting 5 seconds instead of 3 when restarting containers (required for rq workers)

--- mantis/manager.py
+++ mantis/manager.py
@@ -244,8 +244,8 @@ class Mantis(object):
                 CLI.info(f'Removing container [{container}]...')
                 os.system(f'docker {self.docker_ssh} container rm {container}')
 
-                CLI.info(f'Waiting 3 seconds...')
-                sleep(3)
+                CLI.info(f'Waiting 5 seconds...')
+                sleep(5)
 
                 CLI.info(f'Creating new container [{container}]...')
                 os.system(f'docker-compose {self.docker_ssh} -f {self.configs_path}configs/docker/{self.COMPOSE_PREFIX}.yml -f {self.configs_path}configs/docker/{self.COMPOSE_PREFIX}.{self.environment_id}.yml --project-name={self.PROJECT_NAME} run -d --service-ports --name={container} {service}')
